534ac8c3ae0cc9e35c15e2731144eee7bf569b93
ruczak <igor.sarnowski@gmail.com>
2021-10-02 12:48:36 +0200

    Implemented location service

--- App.js
+++ App.js
@@ -1,10 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
-  // let x;
-  // x.toString();
+  const [locServiceEnabled, setLocServiceEnabled] = useState(false);
+  const [loc, setLoc] = useState(null);
+
+  const checkIfLocationEnabled = async () => {
+    let enabled = await Location.hasServicesEnabledAsync();
+
+    if (!enabled) {
+      Alert.alert(
+        'Your location is off',
+        'Please enable your location.',
+        [{ text: 'OK' }],
+        { cancelable: false }
+      );
+    } else setLocServiceEnabled(true);
+  };
+
+  const getCurrentLocation = async () => {
+    let { status } = Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      Alert.alert(
+        'Permission not granted',
+        'Please allow the app to use the location service.',
+        [{ text: 'OK' }],
+        { cancelable: false }
+      );
+    }
+
+    let { coords } = await Location.getCurrentPositionAsync();
+
+    if (coords) {
+      const { latitude, longitude } = coords;
+      setLoc({ latitude, longitude });
+      console.log(coords);
+    }
+  };
+
+  useEffect(() => {
+    checkIfLocationEnabled();
+    getCurrentLocation();
+  }, []);
 
   return (
     <View style={styles.container}>
