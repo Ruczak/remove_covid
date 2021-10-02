cd2deff4d79263d36c416320d0dee7e0762bd00c
ruczak <igor.sarnowski@gmail.com>
2021-10-02 17:54:35 +0200

    Created Checkbox component

---
+++ components/FormEntities/Checkbox.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useState } from 'react';
+import { View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+
+const Checkbox = ({
+  insideStyles,
+  outsideStyles,
+  callback,
+  initial = false
+}) => {
+  const [checked, setChecked] = useState(initial);
+
+  const handlePress = (wasChecked) => {
+    setChecked(!wasChecked);
+    if (callback) callback(!wasChecked);
+    return !wasChecked;
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={() => handlePress(checked)}>
+      <View style={[styles.outline, outsideStyles]}>
+        {checked ? <View style={[styles.inside, insideStyles]} /> : null}
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+const styles = StyleSheet.create({
+  outline: {
+    width: 24,
+    height: 24,
+    borderRadius: 12,
+    borderWidth: 2,
+    borderColor: '#000'
+  },
+  inside: {
+    width: 12,
+    height: 12,
+    borderRadius: 6,
+    backgroundColor: '#ff7712',
+    alignSelf: 'center',
+    marginTop: 4
+  }
+});
+
+export default Checkbox;
