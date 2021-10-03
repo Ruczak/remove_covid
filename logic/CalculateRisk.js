d201cd3372d3cf0d1ca14bf253421f9bbb675c5a
ruczak <igor.sarnowski@gmail.com>
2021-10-03 16:00:33 +0200

    Updated risk calculation function

--- logic/CalculateRisk.js
+++ logic/CalculateRisk.js
@@ -13,10 +13,10 @@ export function calculateRisk(
 ) {
   const sexFactor = sex ? 0.95 : 1.05; // sex => {0: male 1: female}
   const socialDistanceFactor =
-    socialDistance === 0 ? 1.25 : socialDistance === 1 ? 0.75 : 0.5; // socialDistance
-  const maskFactor = mask === 0 ? 1.1 : mask === 1 ? 0.98 : 0.95;
-  const cityFactor = bigCity ? 1.5 : 1;
-  const infectedContactFactor = infectedContact
+    socialDistance === 0 ? 0.75 : socialDistance === 1 ? 0.5 : 1.25; // socialDistance ?
+  const maskFactor = mask === 0 ? 0.95 : mask === 1 ? 0.98 : 1.1; // mask => {0: certified, 1: uncertified, 2: no}
+  const cityFactor = bigCity ? 1.5 : 1; // bigCity ?
+  const infectedContactFactor = infectedContact // infectedMask ? (mask) ?
     ? infectedMask
       ? mask
         ? 1.015
@@ -25,9 +25,9 @@ export function calculateRisk(
       ? 1.7
       : 1.9
     : 1;
-  const houseMembersFactor = houseMembers + 1;
-  const sickHouseMemberFactor = sickHouseMember ? 18 : 1;
-  const sanitizerFactor = sanitizer ? 0.95 : 1.1;
+  const houseMembersFactor = houseMembers + 1; // houseMembers
+  const sickHouseMemberFactor = sickHouseMember ? 18 : 1; // sickHouseMember ?
+  const sanitizerFactor = sanitizer ? 0.95 : 1.1; // sanitizer ?
 
   const riskFactor =
     (currentCases / population) *
