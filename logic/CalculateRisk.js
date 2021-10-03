ff7e4c1eed443850ad4a61fdb291e7519440ae2b
ruczak <igor.sarnowski@gmail.com>
2021-10-03 18:39:42 +0200

    Replaced calculate risk function with new one

--- logic/CalculateRisk.js
+++ logic/CalculateRisk.js
@@ -1,4 +1,4 @@
-export function calculateRisk(
+export function calculateRisk_prev(
   currentCases,
   population,
   sex,
@@ -11,7 +11,7 @@ export function calculateRisk(
   sickHouseMember,
   sanitizer
 ) {
-  const sexFactor = sex === 0 ? 0.95 : 1.05; // sex => {0: male 1: female}
+  const sexFactor = sex === 0 ? 0.95 : 1.05; // sex => {0: male, 1: female}
   const socialDistanceFactor =
     socialDistance === 0 ? 0.75 : socialDistance === 1 ? 0.5 : 1.25; // socialDistance ?
   const maskFactor = mask === 0 ? 0.95 : mask === 1 ? 0.98 : 1.1; // mask => {0: certified, 1: uncertified, 2: no}
@@ -39,6 +39,60 @@ export function calculateRisk(
     houseMembersFactor *
     sickHouseMemberFactor *
     sanitizerFactor;
+
+  return riskFactor;
+}
+
+export function calculateRisk(
+  currentCases,
+  population,
+  sex,
+  socialDistance,
+  mask,
+  bigCity,
+  infectedContact,
+  infectedMask,
+  houseMembers,
+  sickHouseMember,
+  sanitizer,
+  contactFreq
+) {
+  const sexFactor = sex === 0 ? 0 : 1; // sex => {0: male, 1: female}
+  const socialDistanceFactor =
+    socialDistance === 0 ? 0.1 : socialDistance === 1 ? 0 : 0.15; // socialDistance => {0: >= 1 meter, 1: >= 2 meters, 2: No}
+  const maskFactor = mask === 0 ? 0 : mask === 0.02 ? 0 : 0.05; // mask => {0: certified, 1: uncertified, 2: no}
+  const cityFactor = bigCity ? 0.05 : 0; // bigCity ?
+  const infectedContactFactor = infectedContact // infectedMask ? (mask) ?
+    ? infectedMask
+      ? mask
+        ? 0.02
+        : 0.04
+      : mask
+      ? 0.1
+      : 0.15
+    : 0;
+  const houseMembersFactor = [0, 0.02, 0.03, 0.06][houseMembers]; // houseMembers => {0: None, 1: One, 2: Two, 3: Three or more}
+  const sickHouseMemberFactor = sickHouseMember ? 0.15 : 0; // sickHouseMember ?
+  const sanitizerFactor = sanitizer ? 0 : 0.05; // sanitizer ?
+
+  const contactFreqFactor = [0, 0.01, 0.025, 0.04][contactFreq]; // contactFreq => {0: Hardly ever, 1: Once a week, 2: 2-3 times a week, 3: >=4 times a week}
+
+  const seasonFactor = (() => {
+    const month = new Date(Date.now()).getMonth();
+    if (month >= 3 && month <= 8) return 0;
+    return 0.05;
+  })();
+
+  const riskFactor =
+    (currentCases / population) * socialDistanceFactor +
+    sexFactor +
+    maskFactor +
+    cityFactor +
+    infectedContactFactor +
+    houseMembersFactor +
+    sickHouseMemberFactor +
+    sanitizerFactor +
+    contactFreqFactor;
 
   return riskFactor;
 }
